28f842c40f500e95d189a145126fbb07ee4305ec
jose carlos narciso <josecdia@hotmail.com>
2022-11-30 22:56:01 -0300

    Exercicio

--- src/test/java/com/mentorama/javapro/api/test/apitest/controllers/ProductControllerTest.java
+++ src/test/java/com/mentorama/javapro/api/test/apitest/controllers/ProductControllerTest.java
@@ -1,40 +1,61 @@
 package com.mentorama.javapro.api.test.apitest.controllers;
 
-
-import com.mentorama.javapro.api.test.apitest.repositories.IProductRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import com.mentorama.javapro.api.test.apitest.dtos.ProductDTO;
+import com.mentorama.javapro.api.test.apitest.models.Product;
+import com.mentorama.javapro.api.test.apitest.services.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-;
 
-
+import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
-@AutoConfigureMockMvc
-public class ProductControllerTest {
+class ProductControllerTest {
 
-    @Autowired
-    private IProductRepository repository;
+    public static final Integer ID = 12;
+    public static final String PRODUCT_NAME = "Machado de combate";
+    public static final int QUANTITY = 3;
+    public static final double MAX_DISCOUNT = 20.0;
+    public static final double PRICE = 350.0;
 
-    @Autowired
-    private MockMvc mockMvc;
+    Product product;
+    ProductDTO dto;
+
+    @InjectMocks
+    private ProductController controller;
+    @InjectMocks
+    private ProductService service;
 
 
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startProduct();
+    }
 
+    @Test
+    void add() {
+    }
 
+    @Test
+    void update() {
+    }
 
+    @Test
+    void findAll() {
+    }
 
+    @Test
+    void findById() {
+    }
 
-//    @Test
-//    public void shouldAddNewProduct() throws Exception {
-//        String requestBody = "{\"id\":\"10\",\"productName\":Monitor,\"maxDiscount\":2,\"quantity\":5,\"price\":1500.00}";
-//
-//
-//        mockMvc.perform(MockMvcRequestBuilders
-//                        .post("/product")
-//                        .content(requestBody)
-//                        .contentType("application/json"))
-//                .andExpect(status().isCreated());
-//    }
+    @Test
+    void delete() {
+    }
+
+    private void startProduct() {
+        product = new Product(ID, PRODUCT_NAME, QUANTITY, MAX_DISCOUNT, PRICE);
+        dto = new ProductDTO(ID, PRODUCT_NAME, QUANTITY, PRICE);
+    }
 }
